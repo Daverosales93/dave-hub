929d53d7307f07213a1e0ea1cebb8980c22c4f5e
Dave Rosales <davealvarrez93@gmail.com>
2025-05-12 17:58:22 -0600

    Fix: declare aos module correctly

--- src/types/aos.d.ts
+++ src/types/aos.d.ts
@@ -1,9 +1,1 @@
-'use client';
-
-import { useEffect, useState } from "react";
-
-// @ts-ignore
-import AOS from "aos";
-import "aos/dist/aos.css";
-import { Typewriter } from 'react-simple-typewriter';
-import ProjectCard from "@/components/ProjectCard";
+declare module 'aos';
